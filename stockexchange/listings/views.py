be51dd9071528bf74cdd335322e1d900c4fdc7c1
Franck Saounde <franck-arnaud.saoundezekeng@atos.net>
2024-08-22 22:51:00 +0200

    add new views to stockexchange

--- stockexchange/listings/views.py
+++ stockexchange/listings/views.py
@@ -4,3 +4,16 @@ from django.http import HttpResponse
 
 def hello(request):
     return HttpResponse("<h1> Hello Django !</h1>")
+
+
+def about(request):
+    return HttpResponse("<h1> A propos</h1> <p>Nous aimons la bourse de marchandises</p>")
+
+
+def listings(request):
+    return HttpResponse("<h1> Ci après la liste des  marchandises</h1> "
+                        "<p><ul> <li> Element1</li> <li> Element2</li></ul></p>")
+
+
+def contact(request):
+    return HttpResponse("<h1> Nous contacter</h1> <p>Vous pouvez nous joindre via .... </p>")
