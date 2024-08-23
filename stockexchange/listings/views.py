e1d284737b6eeece38df3612adf5d1e0686f4691
Franck Saounde <franck-arnaud.saoundezekeng@atos.net>
2024-08-23 16:54:47 +0200

    add models Bands and Listing

--- stockexchange/listings/views.py
+++ stockexchange/listings/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Band, Listing
+
 
 def hello(request):
-    return HttpResponse("<h1> Hello Django !</h1>")
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1> Hello Django !</h1>
+        <p> Mes groupes préférés sont: </p>
+        <ul> 
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>        
+        </ul>
+    
+    
+    """)
 
 
 def about(request):
@@ -11,8 +24,19 @@ def about(request):
 
 
 def listings(request):
-    return HttpResponse("<h1> Ci après la liste des  marchandises</h1> "
-                        "<p><ul> <li> Element1</li> <li> Element2</li></ul></p>")
+    listings = Listing.objects.all()
+    return HttpResponse(f"""
+        <h1> Ci-après les grands titres pour nos annonces</h1>
+        <p>
+            <ul> 
+                <li> {listings[0].title}</li> 
+                <li> {listings[1].title}</li>
+                <li> {listings[2].title}</li>
+                <li> {listings[3].title}</li>            
+            </ul>
+        </p>
+        
+    """)
 
 
 def contact(request):
