23cdc48e0b215e3d39b4fadf3cdbcb48670cb903
Franck Saounde <franck-arnaud.saoundezekeng@atos.net>
2024-09-01 19:51:27 +0200

    update Listings model with TextChoices and new fields

--- stockexchange/listings/models.py
+++ stockexchange/listings/models.py
@@ -1,10 +1,21 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
-    name2 = models.fields.CharField(max_length=100)
 
 
 class Listing(models.Model):
+    class Type(models.TextChoices):
+        RECORDS = 'REC'
+        CLOTHING = 'CLO'
+        POSTERS = 'POST'
+        MISCELLANEOUS = 'MIS'
+
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2024)],
+                                      blank=True, null=True)
+    type = models.fields.CharField(choices=Type.choices, max_length=5, default='REC')
