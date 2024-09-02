95988f32fffa5beb74683d812a3da499c5fb1d58
Franck Saounde <franck-arnaud.saoundezekeng@atos.net>
2024-09-02 11:23:24 +0200

    update Band model with new fields and register it

--- stockexchange/listings/admin.py
+++ stockexchange/listings/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from listings.models import Band, Listing
+
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year_formed', 'genre')
+
+
+admin.site.register(Band, BandAdmin)
--- stockexchange/listings/models.py
+++ stockexchange/listings/models.py
@@ -3,7 +3,23 @@ from django.db import models
 
 
 class Band(models.Model):
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+        JAZZ = 'JZ'
+
+    def __str__(self):
+        return self.name
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5, default='HH')
+    biography = models.fields.CharField(max_length=1000, default='')
+    year_formed = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2024)],
+        default=2000)
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
 
 
 class Listing(models.Model):
